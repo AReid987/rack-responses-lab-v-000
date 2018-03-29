8b1cb5234c2eb64d1ba7d71533a5ea15adcd670e
Antonio Reid <read.musik@gmail.com>
2018-03-29 03:04:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    
+
     if Time.new(2015,11,27,9,30).hour < 12
       resp.write "Good Morning!"
     elsif Time.new(2015,11,27,14,30).hour > 12
